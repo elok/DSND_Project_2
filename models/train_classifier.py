03c6a92a367978543c09b37c7a4c5e234407c590
Eric Lok <eric.lok@secor-am.com>
2019-10-04 17:08:11 -0400

    added code for the classifier

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,97 @@
 import sys
-
+from sqlalchemy import create_engine
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+# NLP
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+# For ML
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+# For Model Evaluation
+from sklearn.metrics import classification_report, accuracy_score
+# For Saving
+import pickle
 
 def load_data(database_filepath):
-    pass
+    """
+
+    :param database_filepath:
+    :return:
+    """
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_query('SELECT * FROM cleanData', engine)
+
+    X = df['message'].values
+    y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = y.columns
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    """
+
+    :param text:
+    :return:
+    """
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+
+    :return:
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'clf__estimator__min_samples_split': [2, 4],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, n_jobs=4)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+
+    :param model:
+    :param X_test:
+    :param Y_test:
+    :param category_names:
+    :return:
+    """
+    y_preds = model.predict(X_test)
+    print(classification_report(y_preds, Y_test.values, target_names=category_names))
+    print("**** Accuracy scores for each category *****\n")
+    for i in range(36):
+        print("Accuracy score for " + Y_test.columns[i], accuracy_score(Y_test.values[:, i], y_preds[:, i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+
+    :param model:
+    :param model_filepath:
+    :return:
+    """
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
